7bf4004d72f2f4a1b15106c559f0543a213c52b7
Harrison Beckerich <hbeckeri@pi-a004.local>
2016-06-10 23:20:20 -0400

    removed unsafe endpoints

--- example.js
+++ example.js
@@ -9,42 +9,6 @@ app.listen(process.env.PORT, function () {
     console.log('listening on port %s', process.env.PORT);
 });
 
-app.get('/register', (req,  res) => {
-    exampleBot.register();
-});
-
-app.get('/say', function(req, res){
-    var message = req.query.message;
-    exampleBot.echo(message);
-    res.send('OK');
-});
-
-app.get('/users', function(req, res) {
-    exampleBot.getUsers(exampleBot.GROUP_ID, function(json) {
-        res.send(json);
-        exampleBot.updateUsers();
-    });
-});
-
-app.get('/groups', (req, res) => {
-   exampleBot.getGroups()
-       .then((json) => {
-           res.send({success: true, document: json});
-       })
-       .catch((err) => {
-           res.send({error: err});
-       });
-});
-
-app.get('/update', function(req, res) {
-    update();
-    res.send('OK');
-});
-
-app.get('/room', function(req, res) {
-    res.send(exampleBot.GROUP_ID.toString());
-});
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/debug.html');
+app.get('/', (req, res) => {
+   res.send({ name: 'groupme-bot api', version: 'beta 1.0' });
 });
